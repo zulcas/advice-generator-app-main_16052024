81ea3c9012a8efd4d68146fb5f30d18bde1457e8
zulcas <zulemacasadogarcia@gmail.com>
2024-05-16 19:05:13 +0200

    terminado

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // ¡Buena suerte!
+
+//Program variables
+button = document.querySelector(".card__dice");
+let id;
+let advice;
+
+button.addEventListener('click', getAdvice);
+
+
+//Function
+async function getAdvice(){
+    //Call API
+    const response = await fetch ('https://api.adviceslip.com/advice');
+    console.log(response);
+    //Convert JSON
+    const data = await response.json();
+    console.log(data);
+    //Get id and advice
+    document.querySelector('#advice-id').textContent = data.slip.id;
+    document.querySelector('.card__quote').textContent = data.slip.advice;
+};
