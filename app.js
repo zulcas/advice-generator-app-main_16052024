235bcdc05c6859e0bc1c1f67207b6aedfc98a0b6
zulcas <zulemacasadogarcia@gmail.com>
2024-05-16 19:17:28 +0200

    cambios

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 
 //Program variables
 button = document.querySelector(".card__dice");
-let id;
-let advice;
 
 button.addEventListener('click', getAdvice);
 
@@ -17,6 +15,11 @@ async function getAdvice(){
     const data = await response.json();
     console.log(data);
     //Get id and advice
-    document.querySelector('#advice-id').textContent = data.slip.id;
-    document.querySelector('.card__quote').textContent = data.slip.advice;
+    displayAdvice (data.slip.id, data.slip.advice);
+    
 };
+
+function displayAdvice (id, advice){
+    document.querySelector('#advice-id').textContent = id;
+    document.querySelector('.card__quote').textContent = advice;
+}
